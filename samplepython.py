5ba6a5d7f0f8ded8b1c2437acebeaa85d417ab5b
aabbas9-commits <aabbas9@gmail.com>
2026-05-04 11:39:08 -0500

    2nd commit

---
+++ samplepython.py
@@ -0,0 +1,22 @@
+
+
+print("Hello world")
+# print("Hello world")
+print("Hello world")
+print("Hello world")
+
+print("hello world")
+
+from playwright.sync_api import sync_playwright
+
+ 
+
+with sync_playwright() as p:
+
+    browser = p.chromium.launch(headless=False)
+
+    page = browser.new_page()
+
+    page.goto("https://www.amazon.in/")
+
+    page.wait_for_timeout(5000)
